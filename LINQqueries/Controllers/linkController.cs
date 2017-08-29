867c0a9c8e43da654254b82e6c39557b825677f7
vishnu.mechineni <vishnu.mechineni@IRSDSK180>
2017-08-29 16:50:43 +0530

    Add project files.

---
+++ LINQqueries/Controllers/linkController.cs
@@ -0,0 +1,38 @@
+﻿using LINQqueries.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LINQqueries.Controllers
+{
+    public class linkController : Controller
+    {
+        inrdbEntities ie = new inrdbEntities();
+        // GET: link
+        public ActionResult Index()
+        {
+            var x = from n in ie.empc select n;
+            return View(x.ToList());
+        }
+
+        public ActionResult Order()
+        {
+            var x = from n in ie.empc orderby n.sal select n;
+            return View(x.ToList());
+        }
+
+
+        public ActionResult Group()
+        {
+            var results = from p in ie.empc
+                          group p.ename by p.deptno into g
+                          select new Results{ Employees = g.ToList() };
+            return View(results.ToList());
+        }
+
+
+
+    }
+}
---
+++ LINQqueries/Models/Results.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LINQqueries.Models
+{
+    public class Results
+    {
+        //public int? deptno;
+        public List<string> Employees; 
+    }
+}
